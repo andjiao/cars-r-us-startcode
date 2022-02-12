bebcb8168876d02b97427ee74f9d01b36e9ed213
andreajiao <andreajiao@gmail.com>
2022-02-12 14:50:09 +0100

    week2

--- src/test/java/kea/sem3/jwtdemo/service/MemberServiceInMemoryTest.java
+++ src/test/java/kea/sem3/jwtdemo/service/MemberServiceInMemoryTest.java
@@ -1,5 +1,6 @@
 package kea.sem3.jwtdemo.service;
 
+import kea.sem3.jwtdemo.dto.MemberResponse;
 import kea.sem3.jwtdemo.entity.Member;
 import kea.sem3.jwtdemo.repositories.MemberRepository;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -18,7 +23,7 @@ class MemberServiceInMemoryTest {
 
     static MemberService memberService;
 
-    static int mem1ID, mem2ID;
+    static String mem1ID, mem2ID;
 
     @BeforeAll
     static void setUp(@Autowired MemberRepository memberRepository) {
@@ -26,11 +31,21 @@ class MemberServiceInMemoryTest {
 
         memberService = new MemberService(memberRepository);
 
-        //mem1ID= memberRepository.save(new Member("m", "m@d.kl", "nisse", false, ""))
+        mem1ID= memberRepository.save(new Member("hej","mail@k.dk","test123","kurt","niels","3","3",1,true,"23")).getUsername();
+        mem2ID= memberRepository.save(new Member("nisse","ella@k.dk","test123","alfa","jens","kisser","juletræ",30,true,"øl")).getUsername();
+
+
+
     }
 
     @Test
     void getMemebers() {
+        List<MemberResponse> memberResponses = memberService.getMemebers();
+        /*Der forventes to, da der i metoden setUp er blevet instansieret 2 memberobjekter*/
+
+        assertEquals(2,memberResponses.size());
+        assertInstanceOf(MemberResponse.class,memberResponses.get(1));
+        assertThat(memberResponses, containsInAnyOrder(hasProperty("mail", is("ella@k.dk")), hasProperty("mail", is("Up"))));
     }
 
     @Test
--- src/test/java/kea/sem3/jwtdemo/service/MemberServiceMojitoTest.java
+++ src/test/java/kea/sem3/jwtdemo/service/MemberServiceMojitoTest.java
@@ -1,11 +1,16 @@
 package kea.sem3.jwtdemo.service;
 
+import kea.sem3.jwtdemo.entity.Member;
 import kea.sem3.jwtdemo.repositories.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,12 +20,19 @@ class MemberServiceMojitoTest {
     @Mock
     MemberRepository memberRepository;
 
+    @Autowired
+    MemberService memberService;
+
     @BeforeEach
     void setUp() {
+        memberService = new MemberService(memberRepository);
     }
 
     @Test
     void getMemebers() {
+        Mockito.when(memberRepository.findAll()).thenReturn(List.of(
+                //new Member((body,"ella@k.dk","test123","alfa","jens","kisser","juletræ",30,true,"øl"))
+        ));
     }
 
     @Test
